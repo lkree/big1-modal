520b6c56858f659a910b823adfc732d249ee0bbf
lkree <xtendmix@gmail.com>
2019-12-16 00:45:57 +0300

    initial commit

---
+++ basket/precache-manifest.f67a3ec3950521f7252c7895a3e928a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14440044905e3d7c5ec61c2acdb6eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ff2efeede9f2ec6286a",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "fef718ff3cd1a0c8352b",
+    "url": "/static/css/main.32572499.chunk.css"
+  },
+  {
+    "revision": "3ff2efeede9f2ec6286a",
+    "url": "/static/js/2.c0e24ebb.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.c0e24ebb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fef718ff3cd1a0c8352b",
+    "url": "/static/js/main.c3ee7c72.chunk.js"
+  },
+  {
+    "revision": "e44a967cc9e288b0140c",
+    "url": "/static/js/runtime-main.72d7c59a.js"
+  }
+]);
