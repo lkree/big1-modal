df98fa97df5eda0ac44b60b37ca15b6ee38bf1cd
lkree <xtendmix@gmail.com>
2020-01-25 11:31:53 +0300

    peppers-test added

---
+++ peppers-test/precache-manifest.d88a39542919c9f96697372b8deb7d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeb3c811f08ab74fcc21bf03a229655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba2c38feef465bc0700d",
+    "url": "/static/js/2.6290afb3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6290afb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21964767e43dee1e6779",
+    "url": "/static/js/main.2e1d4102.chunk.js"
+  },
+  {
+    "revision": "36deb16c1af4e2362eb1",
+    "url": "/static/js/runtime-main.71924ddb.js"
+  },
+  {
+    "revision": "01fa591572de5b5e342012a83b49a2db",
+    "url": "/static/media/Map.01fa5915.png"
+  }
+]);
