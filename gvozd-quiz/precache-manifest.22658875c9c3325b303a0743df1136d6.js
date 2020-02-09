d7b0c22c2ff68843c7baeb84174c635e844e2be8
lkree <xtendmix@gmail.com>
2020-02-09 11:49:53 +0300

    added gvoz-quiz

---
+++ gvozd-quiz/precache-manifest.22658875c9c3325b303a0743df1136d6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e137d4b4c4e118ea9dc9af3bbe6729dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64afa71d3e6eeac9a1a2",
+    "url": "./static/css/2.547f25e6.chunk.css"
+  },
+  {
+    "revision": "ff09d599b21876ad60dc",
+    "url": "./static/css/main.2e9b2dbe.chunk.css"
+  },
+  {
+    "revision": "64afa71d3e6eeac9a1a2",
+    "url": "./static/js/2.13275817.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.13275817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff09d599b21876ad60dc",
+    "url": "./static/js/main.7596b3a4.chunk.js"
+  },
+  {
+    "revision": "f780cd6884e3ec31434e",
+    "url": "./static/js/runtime-main.a9e9b055.js"
+  },
+  {
+    "revision": "08cddb68db3da8e41a0cea534151abad",
+    "url": "./static/media/FranklinGothicHeavy.08cddb68.woff2"
+  },
+  {
+    "revision": "53fa56bd853ae739b9ac3a6d5f3233bc",
+    "url": "./static/media/FranklinGothicHeavy.53fa56bd.woff"
+  },
+  {
+    "revision": "140d9bbe68af115a92b391271eb07158",
+    "url": "./static/media/bg.140d9bbe.jpg"
+  },
+  {
+    "revision": "6abbfbaab6e5c473837f9fb6ea39f3c4",
+    "url": "./static/media/btn-arrow--gray.6abbfbaa.svg"
+  },
+  {
+    "revision": "f8fdd80d4fea7dd35aad6c72e18f132c",
+    "url": "./static/media/btn-arrow--orange.f8fdd80d.svg"
+  },
+  {
+    "revision": "b495d9c941bcc316d0d2a88f7b96e07a",
+    "url": "./static/media/btn-arrow.b495d9c9.svg"
+  },
+  {
+    "revision": "30bb9346001d6f8433e3d88c98f55ea1",
+    "url": "./static/media/calculator.30bb9346.png"
+  },
+  {
+    "revision": "0f5c57d91e7f8644666c1bf6c3fd5342",
+    "url": "./static/media/final-screen-tick.0f5c57d9.svg"
+  },
+  {
+    "revision": "7c275544c932b6dafc276dc32fd94565",
+    "url": "./static/media/present-icon.7c275544.png"
+  },
+  {
+    "revision": "c863712851bc724c6da4a69e34f04f59",
+    "url": "./static/media/tick.c8637128.svg"
+  }
+]);
