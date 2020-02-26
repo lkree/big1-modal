72eaf297344a9202f5c7e4fd5385d6f3970dadea
lkree <xtendmix@gmail.com>
2020-02-26 18:25:50 +0300

    added netologyRA diplom

---
+++ netologyRA/precache-manifest.4fd7589ef50708c0205cf4abbfaf2655.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbdb7b412338d0f286b73309ec9a75f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8f94d667670ae18dad5",
+    "url": "./static/css/main.20c65363.chunk.css"
+  },
+  {
+    "revision": "064b59734990a34c4eff",
+    "url": "./static/js/2.2a34f6c9.chunk.js"
+  },
+  {
+    "revision": "699fc5e664ec0b1e6f9dedad0ef45093",
+    "url": "./static/js/2.2a34f6c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f94d667670ae18dad5",
+    "url": "./static/js/main.651a8f2d.chunk.js"
+  },
+  {
+    "revision": "f4b4be19e477019eb154",
+    "url": "./static/js/runtime-main.89a5e569.js"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "./static/media/banner.d613d452.jpg"
+  },
+  {
+    "revision": "9c087d4352a142067a9686012eb7ba57",
+    "url": "./static/media/footer-sprite.9c087d43.png"
+  }
+]);
