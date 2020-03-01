8a06c80d78df91e92b5564f6a6d3c9754387445c
lkree <xtendmix@gmail.com>
2020-03-01 11:22:23 +0300

    added aziza product list

---
+++ aziza-product-list/precache-manifest.d90c5e580ef617b73bf94d0a5d7598b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a4b77007c5caf960220eb4b31186e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8392e58d7e464947967",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "23ede1801bc9cb8d9545",
+    "url": "./static/css/main.70eb1306.chunk.css"
+  },
+  {
+    "revision": "d8392e58d7e464947967",
+    "url": "./static/js/2.e870a410.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.e870a410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ede1801bc9cb8d9545",
+    "url": "./static/js/main.e7c9e5be.chunk.js"
+  },
+  {
+    "revision": "d337d063e5bace9aee9e",
+    "url": "./static/js/runtime-main.40c9978a.js"
+  }
+]);
